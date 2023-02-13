4deb88f80359a5a9481a438f641c837984b6e1ec
reizt <reizt.dev@gmail.com>
2023-02-14 08:27:57 +0900

    add: Storybook initを実行後Prettier, ESLintをpassするように編集

---
+++ src/stories/Header.stories.tsx
@@ -0,0 +1,25 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { Header } from './Header';
+
+const meta: ComponentMeta<typeof Header> = {
+  title: 'Example/Header',
+  component: Header,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+export default meta;
+
+const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  user: {
+    name: 'Jane Doe',
+  },
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {};
