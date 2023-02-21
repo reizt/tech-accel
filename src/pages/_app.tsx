cd67e1a07b8c538d537ab1de14778f692fb49f49
reizt <reizt.dev@gmail.com>
2023-02-21 16:03:47 +0900

    add: M PLUS 1フォントを@next/fontから使う

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
+import { M_PLUS_1 } from '@next/font/google';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
+const mPlus1Regular = M_PLUS_1({
+  weight: '400',
+  subsets: ['latin'],
+});
+
 export default function ({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${mPlus1Regular.style.fontFamily};
+          font-weight: ${mPlus1Regular.style.fontWeight!};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
