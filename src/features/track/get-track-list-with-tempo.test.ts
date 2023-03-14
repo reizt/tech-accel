c33a10994d37494e81bc3a4c31e09d78f392df00
cut0 <zeron9421@gmail.com>
2023-03-14 22:57:25 +0900

    add: 曲のリストとテンポを集約する関数の追加

---
+++ src/features/track/get-track-list-with-tempo.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, it, vi, afterEach } from 'vitest';
+import { getTrackListWithTempo } from './get-track-list-with-tempo';
+import * as FetchUserTrackList from './fetch-user-track-list';
+import * as FetchTrackListTempo from './fetch-track-list-tempo';
+
+const mockUserTrackList = [
+  { track: { id: 'A' } },
+  { track: { id: 'B' } },
+  { track: { id: 'C' } },
+  { track: { id: 'D' } },
+  { track: { id: 'E' } },
+];
+
+const mockTempoList = [
+  { id: 'A', tempo: 20 },
+  { id: 'B', tempo: 10 },
+  { id: 'C', tempo: undefined },
+  { id: 'D', tempo: 50 },
+  { id: 'E', tempo: 40 },
+];
+
+describe(getTrackListWithTempo.name, async () => {
+  const fetchUserTrackListSpy = vi.spyOn(FetchUserTrackList, 'fetchUserTrackList');
+  fetchUserTrackListSpy.mockResolvedValue(mockUserTrackList as never);
+
+  const fetchTrackListTempoSpy = vi.spyOn(FetchTrackListTempo, 'fetchTrackListTempo');
+  fetchTrackListTempoSpy.mockResolvedValue(mockTempoList as never);
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('correctly formatted and sorted', async () => {
+    const response = await getTrackListWithTempo({ token: '' });
+    expect(response).toEqual([
+      { track: { id: 'D' }, tempo: 50 },
+      { track: { id: 'E' }, tempo: 40 },
+      { track: { id: 'A' }, tempo: 20 },
+      { track: { id: 'B' }, tempo: 10 },
+    ]);
+  });
+});
--- src/features/track/index.ts
+++ src/features/track/index.ts
@@ -1,2 +1,3 @@
 export * from './fetch-user-track-list';
 export * from './fetch-track-list-tempo';
+export * from './get-track-list-with-tempo';
