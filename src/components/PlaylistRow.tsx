22f653dd26e6d5bb2f6458a300e55cdb66f7e5d0
reizt <reizt.dev@gmail.com>
2023-03-15 20:51:12 +0900

    update: secondsFormatをutilsで関数化

--- src/components/PlaylistRow.tsx
+++ src/components/PlaylistRow.tsx
@@ -1,4 +1,5 @@
-import { ButtonHTMLAttributes, FC, useMemo } from 'react';
+import { formatSeconds } from '#/utils/format-seconds';
+import type { ButtonHTMLAttributes, FC } from 'react';
 
 type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
   playingNow: boolean;
@@ -11,13 +12,6 @@ type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
 };
 
 export const PlaylistRow: FC<Props> = ({ playingNow, serialNumber, song, className = '', ...restProps }) => {
-  const secondsFormat = useMemo(
-    () =>
-      `${Math.floor(song.seconds / 60)
-        .toString()
-        .padStart(2, '0')}:${(song.seconds % 60).toString().padStart(2, '0')}`,
-    [song.seconds],
-  );
   return (
     <button className={`flex h-78 items-center p-16 ${playingNow ? 'bg-white-500' : ''} ${className}`} {...restProps}>
       <span className="w-24 text-left font-bold text-black-600">{serialNumber}</span>
@@ -25,7 +19,7 @@ export const PlaylistRow: FC<Props> = ({ playingNow, serialNumber, song, classNa
         <span className="text-18 leading-snug">{song.name}</span>
         <span className="text-12 leading-snug text-black-600">{song.artistName}</span>
       </span>
-      <span className="text-12 text-black-600">{secondsFormat}</span>
+      <span className="text-12 text-black-600">{formatSeconds(song.seconds)}</span>
     </button>
   );
 };
